3036eb47be422d2b4b8206e53ef889824d45c7bb
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2025-01-01 00:14:01 -0500

    feat: initial rendering for the grid state

--- src/life.rs
+++ src/life.rs
@@ -1,28 +1,32 @@
 use bevy::prelude::*;
-use rand::prelude::*;
 
 #[derive(Resource)]
 pub struct Conway {
 	pub grid: Vec<Vec<LifeCell>>,
 }
 
-#[derive(Component)]
+#[derive(Component, Clone, Hash)]
+pub struct LifeCellRef {
+	pub row: usize,
+	pub col: usize,
+	pub tick: usize,
+}
+
 pub struct LifeCell {
-	pub generation: u32,
+	pub entity: Option<Entity>,
 	pub alive: bool,
 }
 
 impl Conway {
 	pub fn new(size: u8) -> Self {
 		let mut grid = Vec::with_capacity(size as usize);
-		let mut rng = thread_rng();
 
 		for _ in 0..size {
 			let mut col = Vec::with_capacity(size as usize);
 			for _ in 0..size {
 				col.push(LifeCell {
-					generation: 0,
-					alive: if rng.gen_range(1..=6) > 3 { true } else { false },
+					entity: None,
+					alive: false, // if rng.gen_range(1..=6) > 3 { true } else { false },
 				})
 			}
 			grid.push(col);
@@ -32,6 +36,40 @@ impl Conway {
 	}
 
 	pub fn tick(&mut self) {
-		//
+		const NEIGHBOURS: [(isize, isize); 8] = [(-1, 1), (-1, 0), (-1, -1), (0, 1), (0, -1), (1, 1), (1, 0), (1, -1)];
+
+		let rows = self.grid.len();
+		let Some(cols) = self.grid.get(0).map(|items| items.len()) else {
+			return;
+		};
+
+		for row in 0..rows {
+			for col in 0..cols {
+				let mut alive_count = 0;
+
+				for (i, j) in &NEIGHBOURS {
+					let (x, y) = (row as isize + *i, col as isize + *j);
+					if x < 0 || y < 0 || x >= rows as isize || y >= cols as isize {
+						continue;
+					}
+
+					let cell = &self.grid[x as usize][y as usize];
+					if cell.alive {
+						alive_count += 1;
+					}
+				}
+
+				let cell = &mut self.grid[row][col];
+				if cell.alive {
+					if alive_count < 2 || alive_count > 3 {
+						cell.alive = false;
+					}
+				} else {
+					if alive_count == 3 {
+						cell.alive = true;
+					}
+				}
+			}
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod camera;
 mod life;
 
-use bevy::{color::palettes, core_pipeline::tonemapping::DebandDither, prelude::*};
+use std::time::Duration;
+
+use bevy::{color::palettes, core_pipeline::tonemapping::DebandDither, prelude::*, time::common_conditions::on_timer};
 use camera::{orbit, CameraSettings};
-use life::Conway;
+use life::{Conway, LifeCell, LifeCellRef};
+use rand::prelude::*;
 
 fn main() {
 	let mut app = App::new();
@@ -20,14 +23,32 @@ fn main() {
 
 	app.insert_resource(ClearColor(Color::BLACK));
 	app.init_resource::<CameraSettings>();
+	app.init_resource::<Handles>();
 
 	app.add_systems(Startup, setup);
-	app.add_systems(Update, orbit);
+	app.add_systems(
+		Update,
+		(orbit, tick_simulation.run_if(on_timer(Duration::from_millis(200)))),
+	);
 
 	app.run();
 }
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+#[derive(Resource, Default)]
+struct Handles {
+	material: Handle<StandardMaterial>,
+	mesh: Handle<Mesh>,
+}
+
+const CUBE_SIZE: f32 = 1.;
+const GRID_SIZE: usize = 20;
+
+fn setup(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+	mut handles: ResMut<Handles>,
+) {
 	// Camera
 	commands.spawn((
 		Camera3d::default(),
@@ -52,27 +73,93 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 		Transform::from_xyz(8.0, 16.0, 8.0),
 	));
 
-	// Conway board
-	let conway = Conway::new(4);
 	let alive_material = StandardMaterial {
 		base_color: palettes::tailwind::ZINC_200.into(),
 		..Default::default()
 	};
 	let material_handle = materials.add(alive_material);
-	let mut grid_bundles = Vec::new();
-	let cube_size = 1.;
+	let mesh_handle = meshes.add(Cuboid::from_length(CUBE_SIZE));
+	handles.material = material_handle.clone();
+	handles.mesh = mesh_handle.clone();
 
-	for (row, items) in conway.grid.iter().enumerate() {
-		for (col, cell) in items.iter().enumerate() {
-			if !cell.alive {
-				continue;
-			}
-			grid_bundles.push((
-				Mesh3d(meshes.add(Cuboid::from_length(cube_size))),
-				MeshMaterial3d(material_handle.clone()),
-				Transform::from_xyz(row as f32 * cube_size, 0., col as f32 * cube_size),
-			))
+	// Conway board
+	let mut conway = Conway::new(GRID_SIZE as u8);
+	let mut rng = thread_rng();
+	for row in 0..GRID_SIZE {
+		for col in 0..GRID_SIZE {
+			let alive = if rng.gen_range(1..=6) > 3 { true } else { false };
+			let entity = commands
+				.spawn((
+					LifeCellRef { row, col, tick: 0 },
+					Mesh3d(mesh_handle.clone()),
+					MeshMaterial3d(material_handle.clone()),
+					Transform::from_xyz(row as f32 * CUBE_SIZE, 0., col as f32 * CUBE_SIZE),
+					if alive { Visibility::Visible } else { Visibility::Hidden },
+				))
+				.id();
+			conway.grid[row][col] = LifeCell {
+				entity: Some(entity),
+				alive,
+			};
 		}
 	}
-	commands.spawn_batch(grid_bundles);
+
+	commands.insert_resource(conway);
+}
+
+fn tick_simulation(
+	mut commands: Commands,
+	mut q_cells: Query<(&mut LifeCellRef, &mut Transform, &mut Visibility)>,
+	mut conway: ResMut<Conway>,
+	handles: Res<Handles>,
+) {
+	// Before tick, clone the current state of the board
+	for (cell, _, visibility) in q_cells.iter_many(
+		conway
+			.grid
+			.iter()
+			.flat_map(|cells| cells)
+			.filter_map(|cell| cell.entity),
+	) {
+		if let Visibility::Hidden = visibility {
+			continue; // Skip hidden cells
+		}
+
+		let LifeCellRef { row, col, .. } = cell.clone();
+		commands.spawn((
+			LifeCellRef { row, col, tick: 1 },
+			Mesh3d(handles.mesh.clone()),
+			MeshMaterial3d(handles.material.clone()),
+			Transform::from_xyz(row as f32 * CUBE_SIZE, -1., col as f32 * CUBE_SIZE),
+			Visibility::Visible,
+		));
+	}
+
+	conway.tick();
+
+	// After tick, update the state of the top slice of blocks
+	let mut iter = q_cells.iter_many_mut(
+		conway
+			.grid
+			.iter()
+			.flat_map(|cells| cells)
+			.filter_map(|cell| cell.entity),
+	);
+	while let Some((cell_ref, _, mut visibility)) = iter.fetch_next() {
+		let cell = &conway.grid[cell_ref.row][cell_ref.col];
+
+		if cell.alive && visibility.as_ref() == Visibility::Hidden {
+			*visibility = Visibility::Visible;
+		} else if !cell.alive && visibility.as_ref() == Visibility::Visible {
+			*visibility = Visibility::Hidden;
+		}
+	}
+
+	// Shift all states downwards
+	for (mut cell, mut transform, _) in q_cells.iter_mut() {
+		if cell.tick >= 1 {
+			transform.translation.y -= 1.;
+			cell.tick += 1;
+		}
+	}
 }
